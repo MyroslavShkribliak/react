58e32298ccfd89fcbe423b0d91bf0b9d68630444
SK1LL32 <myroslav191@gmail.com>
2022-09-14 00:29:30 +0300

    Install Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
 import {simpsons} from './components/data'
-// import Ccomponents from "./components/Components";
 import Components from "./components/Components";
 import Simson from "./components/Simson";
 
