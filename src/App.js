661d115ceca027a46edfe0397d715f338b1e5646
SK1LL32 <myroslav191@gmail.com>
2022-09-17 19:17:13 +0300

    Install Commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
+// import logo from './logo.svg';
 import './App.css';
-import {simpsons} from './components/data'
-import Components from "./components/Components";
-import Simson from "./components/Simson";
 
 function App() {
     return (
-        <div>
-            {
-                simpsons.map(value => (<Simson key={value.name} item={value}/>))
-            }
-            <Components/>
-        </div>
+        <div></div>
+
     );
 }
 
