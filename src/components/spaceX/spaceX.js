2a1a1bdb930b12214a7559b1603b733bea156989
SK1LL32 <myroslav191@gmail.com>
2022-09-19 17:32:46 +0300

    Install Commit

--- src/components/spaceX/spaceX.js
+++ src/components/spaceX/spaceX.js
@@ -4,8 +4,8 @@ export function SpaceX(props) {
     return(
         <div>
             <h2>Mission name - {launch.mission_name}</h2>
-            <p>Launch year - {launch.launch_year} <br/>
-                Mission patch - <a href={launch.links.mission_patch_small}>{launch.links.mission_patch_small}</a></p>
+            <p>Launch year - {launch.launch_year}</p>
+            <img src={launch.links.mission_patch_small} alt={launch.flight_number}/>
         </div>
     )
 }
